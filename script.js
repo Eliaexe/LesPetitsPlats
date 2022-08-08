01c0e5de1f669a68dbafddc5f02b03220dccec9d
Eliaexe <79944871+Eliaexe@users.noreply.github.com>
2022-08-08 17:10:41 +0200

    last update

--- script.js
+++ script.js
@@ -1,112 +1,168 @@
-function fetchData() {
-fetch('data.json')
-    .then(response => {
-        if (!response.ok) {
-            throw Error('ERROR');
-        }
-        return response.json();
-    })
-    .then(data => {
-        //console.log(data)
+let cardContainer = document.getElementById('cardContainer');
 
+// fetch
+const loadfoods = async () => {
+  try {
+      const res = await fetch('recipes.json');
+      data = await res.json();
+      let food = data.recipes;
+      //console.log(food); 
+      //showRecipes(food)
+      search(food)
+  } catch (err) {
+      console.error(err);
+  }
+};
 
-        let search = document.getElementById('search');
-        let name = data.map(e => e.name)
-        let ingr = data.map(e => e.ingredients)
-        let descr = data.map(e => e.description)
-        let appareils = data.map(e => e.appliance)
-        let ustensils = data.map(e => e.ustensils)
-        var allIngr = []
+loadfoods()
 
-        for (let i = 0; i < ingr.length; i++) {
-            const el = ingr[i];
-            let single = el.map(e => e.ingredient)
-
-            for (let i = 0; i < single.length; i++) {
-                const el = single[i];
-                allIngr.push(single[i].toLowerCase())
-            }
-        }
-
-        var uniqueChars = [...new Set(allIngr)];
-        var nameArr = name.map(n => n.toLowerCase());
-
-        let ing = uniqueChars.sort();
-        let nom = nameArr
-        let des = descr
-        let appa = appareils.sort();
-        let utns = ustensils.sort();
-
-            function matches (pat, str) {
-                str = str.toLowerCase()
-                return str.match(pat)
-            }
-
-        search.addEventListener('keyup', () => {
-            let val = search.value.toLowerCase();
-            let all = [ing, nom, des]
-            var regEx = new RegExp('(\\w*'+val+'\\w*)','gi');
-            
-
-
-            function getObj(val){
-                for (const z in data) {
-                    let y = data[z];
-                    for (const k in y) {
-                        console.log(y[k]);
-
-                    }
-                }
-            }
-
-            if(val.length >= 3){
-                for (const e of all){
-                    let y = []
-                    console.clear()
-                    for (let i = 0; i < e.length; i++) {
-                        const el = e[i];
-                        let x = matches(regEx,el);
-                        console.log(x);
-                        if (x !== null){
-                            getObj(el)
-                        }
-                    }
-                }
-            }
-        })
-
-        let in1 = document.getElementById('blue')
-        let in2 = document.getElementById('green')
-        let in3 = document.getElementById('red')
-
-        let allIn = [in1, in2, in3]
-
-        allIn.forEach(e => {
-            e.addEventListener('keyup', () => {
-                let val = (e.value).toLowerCase();
-                var regEx = new RegExp('(\\w*'+val+'\\w*)','gi');
-
-                if (e == in1) {
-                    ing.forEach(el => {
-                        console.log(matches(regEx, el));
-                    });
-                } else if(e == in2){
-                    appa.forEach(ele => {
-                        console.log(matches(regEx, ele));
-                    });
-                } else if (e == in3){
-                    utns.forEach(em => {
-                        console.log(matches(regEx, em));
-                    });
-                }
-        })
-
-});
-
-
-})
+//get all the single recepies
+function showRecipes(e){
+  for (let i = 0; i < e.length; i++) {
+    const el = e[i];
+    displayCard(el)
+  }
 }
 
-fetchData();
+//clean the card container
+function cleanRecepies() {
+    cardContainer.innerHTML = ``
+}
+
+//display all the card disponible
+function displayCard(obj) {
+  let cardContainer = document.getElementById('cardContainer');
+  let card = `
+  <div class="post ${obj.id}">
+      <div class="top_post"></div>
+      <div class="pad">
+          <div class="middle_post">
+              <div class="post_title">${obj.name}</div>
+              <div class="post_time">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M232 120C232 106.7 242.7 96 256 96C269.3 96 280 106.7 280 120V243.2L365.3 300C376.3 307.4 379.3 322.3 371.1 333.3C364.6 344.3 349.7 347.3 338.7 339.1L242.7 275.1C236 271.5 232 264 232 255.1L232 120zM256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0zM48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48C141.1 48 48 141.1 48 256z"/></svg>
+                  <p class="post_number">${obj.time}</p>
+              </div>
+          </div>
+          <div class="bottom_post">
+              <ul class="ul_post" id="${obj.id}">
+
+              </ul>
+              <p class="description">${obj.description}</p>
+          </div>
+      </div>
+  </div>
+  `
+  cardContainer.innerHTML += card
+  //display single ingredients in his card
+  let ingredients = obj.ingredients
+  for (let i = 0; i < ingredients.length; i++) {
+    const ele = ingredients[i];
+    document.getElementById(obj.id).innerHTML += `<li>${ele.ingredient}: ${ele.quantity} ${ele.unit}</li>`
+  }
+}
 
 
+
+function displayResoults(e, place, names) {
+
+    let toListPosition = document.querySelectorAll(`[placeholder=${place}]`)[0].parentNode.parentNode; //.children
+    let list = toListPosition.childNodes[3];
+    let parentList = list.parentNode.classList;
+    let allIcons = document.querySelectorAll("[alt='x-icon']")
+    // let disponible = document.getElementsByClassName('disponible')
+
+    for (let i = 0; i < names.length; i++) {
+        const e = names[i];
+        // console.log(list.childNodes);
+        if (list.innerHTML.includes(e) == false && list.childNodes.length < 30) {
+            list.innerHTML += `<li>${e}</li>`
+        }
+        console.log();
+    }
+
+    for (let i = 0; i < list.childNodes.length; i++) {
+        const singleResoult = list.childNodes[i];
+        let icon = `<img src="/img/remove-icon.png" alt="x-icon">`
+        
+        
+        // click on the list item
+        singleResoult.addEventListener('click', () => {
+            let selectedListed = document.getElementById('specificSelected')
+            if (selectedListed.innerHTML.toLowerCase().includes(singleResoult) == false) {
+                // give the bg color
+                for (let i = 0; i < parentList.length; i++) {
+                    const e = parentList[i];
+                    if (e.includes('blue')) {
+                        singleResoult.classList.add('blue--bg')
+                    } else if (e.includes('green')){
+                        singleResoult.classList.add('green--bg')
+                    } else if (e.includes('red')){
+                        singleResoult.classList.add('red--bg')
+                    }
+                }
+                let clone = singleResoult.cloneNode(true)
+                // transfer item to top
+                selectedListed.appendChild(clone)  
+                clone.innerHTML += icon
+                //close item to top
+                clone.childNodes[1].addEventListener('click', () => {
+                    clone.remove()
+                })
+            }
+        })
+        
+    }
+
+
+
+
+    // // for (let i = 0; i < toListPosition.length; i++) {
+    // //     const e = toListPosition[i];
+    // //     console.log(e);
+    // //     }
+    // //     if (document.getElementById(e.id) == null) {
+    // //         displayCard(e)        
+    // //     }    
+}
+
+
+
+
+function search (e) {
+  e.map((food) => {
+    const {id, name, time, description, ingredients, appliance, ustensils} = food
+    let ingredient = ingredients.map((x) => {
+        return x.ingredient
+    })
+    //console.log(id);
+    let input = document.getElementsByClassName('input');
+    //console.log(input);
+        for (let i = 0; i < input.length; i++) {
+            const el = input[i];
+            el.addEventListener('keyup', () => {
+                //console.log(el.value, el.placeholder);
+                let searchString = el.value.toLowerCase().trim()
+                //console.log(searchString);
+                if (el.value.length >= 3) {
+                    if (el.placeholder == 'Ingredients' && 
+                        ingredient.join(',').toLowerCase().includes(searchString)) {
+                        displayResoults(food, el.placeholder, ingredient)
+
+                    } else if (el.placeholder == 'Appareils' && appliance.toLowerCase().includes(searchString)) {
+                        displayResoults(food, el.placeholder, appliance);
+                    } else if (el.placeholder == 'Utensiles' && ustensils.join(',').toLowerCase().includes(searchString)) {
+                        displayResoults(food, el.placeholder, ustensils);
+
+                    } else if (el.placeholder == 'Rechercher un ingredient, une recette, etc...' && (
+                                food.name.toLowerCase().includes(searchString) ||
+                                food.description.toLowerCase().includes(searchString) ||
+                                ingredients.find(o => o.ingredient.toLowerCase().includes(searchString)))){
+                        displayResoults(food);
+                    }
+                } else {
+                    console.log('nope');
+                }
+            })
+        }    
+    })
+}
