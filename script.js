7be0944ab3f7c216539a4c936d307be1c7c8569a
Elia Sidori <eliasidori@outlook.it>
2022-09-01 10:36:57 +0200

    now the tag shows and return an array of the recepies obj. need to display the resoult but need to fix when you delate the tag

--- script.js
+++ script.js
@@ -2,61 +2,60 @@ let cardContainer = document.getElementById('cardContainer');
 let input = document.getElementsByClassName('input');
 let listForSearch = document.getElementById('specificSelected')
 
-
 // fetch
 const loadfoods = async () => {
-  try {
-      const res = await fetch('recipes.json');
-      let data = await res.json();
-      let food = data.recipes;
-      search(food)      
-      showRecipes(food)
-      dropDown(food)
-  } catch (err) {
-      console.error(err);
-  }
+    try {
+        const res = await fetch('recipes.json');
+        let data = await res.json();
+        let food = data.recipes;
+        search(food)      
+        showRecipes(food)
+        dropDown(food)
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 loadfoods()
 
 //get all the single recepies
 function showRecipes(e){
-  for (let i = 0; i < e.length; i++) {
-    const el = e[i];
-    displayCard(el)
-  }
+    for (let i = 0; i < e.length; i++) {
+        const el = e[i];
+        displayCard(el)
+    }
 }
 
 //display all the card disponible
 function displayCard(obj) {
-  let cardContainer = document.getElementById('cardContainer');
-  let card = `
-  <article class="post" id="${obj.id}">
-      <div class="top_post"></div>
-      <div class="pad">
-          <div class="middle_post">
-              <div class="post_title">${obj.name}</div>
-              <div class="post_time">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M232 120C232 106.7 242.7 96 256 96C269.3 96 280 106.7 280 120V243.2L365.3 300C376.3 307.4 379.3 322.3 371.1 333.3C364.6 344.3 349.7 347.3 338.7 339.1L242.7 275.1C236 271.5 232 264 232 255.1L232 120zM256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0zM48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48C141.1 48 48 141.1 48 256z"/></svg>
-                  <p class="post_number">${obj.time}</p>
-              </div>
-          </div>
-          <div class="bottom_post">
-              <ul class="ul_post" >
-              <div class="prova"></div>
-              </ul>
-              <p class="description">${obj.description}</p>
-          </div>
-      </div>
-  </article>
-  `
-  cardContainer.innerHTML += card
-  //display single ingredients in his card
-  let ingredients = obj.ingredients
-  for (let i = 0; i < ingredients.length; i++) {
-    const ele = ingredients[i];
-    document.getElementById(obj.id).querySelector('.ul_post').innerHTML += `<li>${ele.ingredient}: ${ele.quantity} ${ele.unit}</li>`
-  }
+    let cardContainer = document.getElementById('cardContainer');
+    let card = `
+    <article class="post" id="${obj.id}">
+        <div class="top_post"></div>
+        <div class="pad">
+            <div class="middle_post">
+                <div class="post_title">${obj.name}</div>
+                <div class="post_time">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M232 120C232 106.7 242.7 96 256 96C269.3 96 280 106.7 280 120V243.2L365.3 300C376.3 307.4 379.3 322.3 371.1 333.3C364.6 344.3 349.7 347.3 338.7 339.1L242.7 275.1C236 271.5 232 264 232 255.1L232 120zM256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0zM48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48C141.1 48 48 141.1 48 256z"/></svg>
+                    <p class="post_number">${obj.time}</p>
+                </div>
+            </div>
+            <div class="bottom_post">
+                <ul class="ul_post" >
+                <div class="prova"></div>
+                </ul>
+                <p class="description">${obj.description}</p>
+            </div>
+        </div>
+    </article>
+    `
+    cardContainer.innerHTML += card
+    //display single ingredients in his card
+    let ingredients = obj.ingredients
+    for (let i = 0; i < ingredients.length; i++) {
+        const ele = ingredients[i];
+        document.getElementById(obj.id).querySelector('.ul_post').innerHTML += `<li>${ele.ingredient}: ${ele.quantity} ${ele.unit}</li>`
+    }
 }
 
 //dropdown open and close
@@ -79,39 +78,44 @@ function dropDown(food){
     }
 }
 
-//return the specific item in dropdown list
-function sortSpecific(food, place) {
+function relevantRecepies(food) {
     let relevant = document.querySelectorAll('article:not(.d-none)')
     let relevantId = Array.from(relevant, x => x.id)
     let lookingFor = relevantId.map((x) => { return food.find(element => element.id == x) })
+    return lookingFor
+}
+
+//return the specific item in dropdown list
+function sortSpecific(food, place) {
+    let iFindThis = relevantRecepies(food)
     if (place == 'Ingredients') {
-        let ingrObj = Array.from(lookingFor, x => x.ingredients)
+        let ingrObj = Array.from(iFindThis, x => x.ingredients)
         let ingrArr = ingrObj.map((x) => {return Array.from(x, y => y.ingredient)})
         let allIngredient = ingrArr.flatMap(x => x)
-        return refreshDropDown([...new Set(allIngredient)], place)       
+        return refreshDropDown([...new Set(allIngredient)], place, food)       
     } else if (place == 'Appareils') {
-        let applianceArr = Array.from(lookingFor, x => x.appliance)
-        return refreshDropDown([...new Set(applianceArr)], place)
+        let applianceArr = Array.from(iFindThis, x => x.appliance)
+        return refreshDropDown([...new Set(applianceArr)], place, food)
     } else if (place == 'Utensiles') {
-        let utensilesArr = Array.from(lookingFor, x => x.ustensils)
+        let utensilesArr = Array.from(iFindThis, x => x.ustensils)
         let allUtensiles = utensilesArr.flatMap(x => x)
-        return refreshDropDown([...new Set(allUtensiles)], place)
+        return refreshDropDown([...new Set(allUtensiles)], place, food)
     }
 }
 
 //display the specific item in dropdown list
-function refreshDropDown(arr, place) {
+function refreshDropDown(arr, place, food) {
     let putHere = document.querySelector(`[placeholder=${place}]`).parentNode.parentNode.children[1]
     putHere.innerHTML = ``
     for (let i = 0; i < arr.length; i++) {
         const e = arr[i];
         putHere.innerHTML += `<li>${e}</li>`
     }
-    addAndSearch(putHere, place)
+    addAndSearch(putHere, food)
 }
 
 //take the dropdown element clicked and copy to the top list for sort
-function addAndSearch(place, name) {
+function addAndSearch(place, food) {
     let listToListen = place.children
     let background = place.parentElement.classList[1]
     for (let i = 0; i < listToListen.length; i++) {
@@ -119,11 +123,22 @@ function addAndSearch(place, name) {
         e.addEventListener('click', (event) => {
             if (listForSearch.innerText.includes(e.innerHTML) == false) {
                 listForSearch.innerHTML += `<li class="${background}">${e.innerHTML}<img src="./img/remove-icon.png"></img></li>`
+                menageFilter();
+                searchWhitTags(food, background, e.innerHTML);
                 // console.log(name, e.innerHTML);
             }
         })
     }
-    menageFilter();
+}
+// remove tag if click on x img
+function menageFilter() {
+    let tags = listForSearch.childNodes
+    for (let i = 0; i < tags.length; i++) {
+        const e = tags[i].childNodes[1];
+        e.addEventListener('click', () =>{
+            e.parentElement.remove()
+        })
+    }
 }
 
 //all recepies must be visible
@@ -134,6 +149,23 @@ function resetRecepies() {
             element.classList.remove('d-none')
         }
     } 
+}
+
+function searchWhitTags(food, background, element) {
+    let value = relevantRecepies(food)
+    // console.log(background);
+    let mapping = value.map((food) => {
+        const {id, name, time, description, ingredients, appliance, ustensils} = food 
+        if (background == 'blue--bg' && ingredients.find(o => o.ingredient.toLowerCase().includes(element.toLowerCase()))) {
+            return food
+        } else if (background == 'green--bg' && appliance.toLowerCase() == element.toLowerCase()){
+            return food 
+        } else if (background == 'red--bg' && ustensils.includes(element)){
+            return food 
+        }
+    })
+    let data = mapping.filter(Boolean)
+    console.log(data);
 }
 
 //search based on user input
